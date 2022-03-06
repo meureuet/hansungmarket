86731c57b1556207c535b5cf8ed24ca811e78916
meureuet <gmdgjfr@gmail.com>
2022-03-06 23:10:30 +0900

    일대다 중복결과 distinct

--- src/main/java/com/hansungmarket/demo/repository/board/BoardRepositoryImpl.java
+++ src/main/java/com/hansungmarket/demo/repository/board/BoardRepositoryImpl.java
@@ -17,7 +17,7 @@ public class BoardRepositoryImpl implements BoardRepositoryCustom{
     @Override
     public List<Board> findByUsername(String username) {
 
-        return null; // 나중에 고칠 것
+        return null; // 나중에 수정
     }
 
     @Override
